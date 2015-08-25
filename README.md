272560faf208b3cf142d5dab3416381f2703e7b6
Diego Mesa <diego898@gmail.com>
2015-08-25 12:21:23 -1000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Introduction to IPython
+
+An Introduction to IPython given in Hawaii on 08/25/15
---
+++ helper_code/conf_mat_helper.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_confusion_matrix(cm, title='Confusion matrix', cmap=plt.cm.Blues):
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(iris.target_names))
+    plt.xticks(tick_marks, iris.target_names, rotation=45)
+    plt.yticks(tick_marks, iris.target_names)
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
